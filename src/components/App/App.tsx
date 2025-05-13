1e848d96dd719d46002ee47ba8680c03f488aa0b
Svitlana-Titska <s.titskaya22@gmail.com>
2025-05-13 21:54:26 +0200

    add

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
-import { type Votes, type VoteType } from "./types/votes";
-import CafeInfo from "./components/CafeInfo/CafeInfo.tsx";
-import VoteOptions from "./components/VoteOptions/VoteOptions";
-import VoteStats from "./components/VoteStats/VoteStats";
-import Notification from "./components/Notification/Notification";
+import { type Votes, type VoteType } from "../../types/votes.ts";
+import CafeInfo from "../CafeInfo/CafeInfo.tsx";
+import VoteOptions from "../VoteOptions/VoteOptions.tsx";
+import VoteStats from "../VoteStats/VoteStats.tsx";
+import Notification from "../Notification/Notification.tsx";
 import css from "./App.module.css";
 
 export default function App() {
